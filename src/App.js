3240316e63f1f74a3fbbec5dda97bdfd949f3138
ThaoHuynhD <thaohuynh34.678@gmail.com>
2023-08-31 23:42:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import GlassLayout from "./Layout/GlassLayout";
+import ModelLayout from "./Layout/ModelLayout";
+import RenderWithMap from "./RenderWithMap/RenderWithMap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <ModelLayout />
+        <GlassLayout />
+      </div>
     </div>
   );
 }
---
+++ src/Layout/GlassLayout.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import RenderWithMap from "../RenderWithMap/RenderWithMap";
+
+export default class GlassLayout extends Component {
+  render() {
+    return <RenderWithMap />;
+  }
+}
